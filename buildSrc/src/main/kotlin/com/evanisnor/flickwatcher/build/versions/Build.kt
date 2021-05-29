652005c78940af4bee6d7d5c0352f43f87232c8d
Evan Isnor <ewisnor@gmail.com>
2021-05-29 16:19:58 -0300

    Version 0.0.2

--- buildSrc/src/main/kotlin/com/evanisnor/flickwatcher/build/versions/Build.kt
+++ buildSrc/src/main/kotlin/com/evanisnor/flickwatcher/build/versions/Build.kt
@@ -3,8 +3,8 @@ package com.evanisnor.flickwatcher.build.versions
 object Build {
 
     object Flickwatcher {
-        const val versionCode = 2
-        const val versionName = "0.0.1"
+        const val versionCode = 3
+        const val versionName = "0.0.2"
     }
 
     object Android {
@@ -12,11 +12,6 @@ object Build {
         const val minSdk = 28
         const val compileSdk = 30
         const val targetSdk = compileSdk
-        const val gradlePlugin = "com.android.tools.build:gradle:7.0.0-beta03"
-    }
-
-    object Kotlin {
-        const val gradlePlugin = "org.jetbrains.java:java-gradle-plugin:1.4.32"
     }
 
 }
